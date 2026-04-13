d30aa26cd51f2d78064cd41fbee70f43154e845a
Sarvagya Rastogi <123718724+SarvagyaRastogi7@users.noreply.github.com>
2026-04-14 00:46:23 +0530

    Delete src/covenanttrackingphase1 directory

--- src/covenanttrackingphase1/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Allow `python -m covenanttrackingphase1` (use with PYTHONPATH=src if the package isn't installed)."""
-
-from covenanttrackingphase1.main import run
-
-if __name__ == "__main__":
-    run()
--- src/covenanttrackingphase1/tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from .covenant_tools import ComputeCovenantComplianceTool, ReadCovenantWorkbookTool
-
-__all__ = ["ReadCovenantWorkbookTool", "ComputeCovenantComplianceTool"]
